e469ea11d7f2ac72d197e3b66a4c9e4733354b3d
Peter Örneholm <peter@orneholm.com>
2020-02-06 14:06:46 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RadioText.net
+
+[![Build Status](https://dev.azure.com/orneholm/RadioText.net/_apis/build/status/RadioText.net?branchName=master)](https://dev.azure.com/orneholm/RadioText.net/_build/latest?definitionId=10&branchName=master)
+[![Live demo](https://img.shields.io/static/v1?label=Demo&message=Try%20out%20the%20live%20demo&color=#17547a)](https://RadioText.net/)
+[![Twitter Follow](https://img.shields.io/badge/Twitter-@PeterOrneholm-blue.svg?logo=twitter)](https://twitter.com/PeterOrneholm)
---
+++ src/Orneholm.RadioText/TransferedEpisode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Orneholm.SverigesRadio.Api.Models.Response.Episodes;
+
+namespace Orneholm.NewsSearch
+{
+    public class TransferedEpisode
+    {
+        public Episode Episode { get; set; }
+        public string EpisodeAudioLocale { get; set; }
+        public string EpisodeBlobIdentifier { get; set; }
+        public Uri OriginalAudioUri { get; set; }
+        public string OriginalAudioExtension { get; set; }
+        public Uri BlobAudioAuthenticatedUri { get; set; }
+        public Uri BlobAudioUri { get; set; }
+    }
+}
